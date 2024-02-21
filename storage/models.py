df4f02a9d8557dbd876435a30c4e53ab20e96dca
redfox193 <cer2005dima@gmail.com>
2024-02-21 15:47:46 +0300

    Add list of subjects

--- storage/models.py
+++ storage/models.py
@@ -1,10 +1,44 @@
 from django.db import models
 from enumchoicefield import ChoiceEnum, EnumChoiceField
+from django.db import models
 
 
 class Subjects(ChoiceEnum):
-    linal = "Линейная алгебра"
-    ma = "Математический анализ"
+    aisd = "АиСД (ТА)"
+    aitch = "АиТЧ"
+    angl = "Английский язык"
+    bzch = "БЖЧ"
+    vkad = "ВКиАД"
+    vma = "ВМА"
+    dii = "ДиИИ (МА)"
+    dmiml = "ДМиМЛ"
+    du = "ДУ"
+    iso = "ИСО"
+    hist = "История"
+    ks = "КС"
+    mv = "МВ"
+    subd = "МДиСУБД"
+    mop = "МОП"
+    mo = "МО"
+    mt = "МТ" 
+    ni = "НИ"
+    oviag = "ОВиАГ (ГА)"
+    oimp = "ОиМП"
+    os = "ОС"
+    pp = "ПП"
+    rifk = "РиФКА"
+    rkpp = "РКПП"
+    tvims = "ТВиМС"
+    tm = "ТерМех (ТМ)"
+    tfkp = "ТФКП"
+    tp = "ТП"
+    umf = "УМФ (УРЧП)"
+    faiiu = "ФАиИУ"
+    ma = "ЧиФР"
+    chma = "ЧМ (МЧА)"
+    chmmf = "ЧММФ"
+    econ = "Экономика"
+    other = "Другое"
 
 
 class Categories(ChoiceEnum):
@@ -20,6 +54,7 @@ class File(models.Model):
     author = models.CharField(max_length=100, null=False)
     category = EnumChoiceField(enum_class=Categories, null=False)
     file = models.FileField(upload_to='files/')
+
     
     def __str__(self) -> str:
         return self.name
