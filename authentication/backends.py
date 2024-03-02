b1d19180ff887e2999c0fbc9b3a9ca70e007c254
redfox193 <cer2005dima@gmail.com>
2024-03-02 22:58:03 +0300

    fix: change token type to bearer

--- authentication/backends.py
+++ authentication/backends.py
@@ -8,7 +8,7 @@ from .models import User
 
 
 class JWTAuthentication(authentication.BaseAuthentication):
-    authentication_header_prefix = 'Token'
+    authentication_header_prefix = 'Bearer'
 
     def authenticate(self, request):
         request.user = None
